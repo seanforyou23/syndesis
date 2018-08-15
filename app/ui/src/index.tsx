ba44689479bfb6810d18e5a811c664e21e42bc76
Michael Spaxman <seanforyou23@gmail.com>
2018-08-14 22:06:36 -0400

    [issue-3225] set tslint and tsconfig along with webpack dev server

--- app/ui/src/index.tsx
+++ app/ui/src/index.tsx
@@ -1,1 +1,28 @@
-console.log('index.tsx');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export interface Props {
+  name: string;
+  enthusiasmLevel?: number;
+}
+
+export function HelloReactTypeScript({name, enthusiasmLevel = 1}: Props) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error('You could be a little more enthusiastic. :D');
+  }
+
+  return (
+    <div>
+      <h1>{name + getExclamationMarks(enthusiasmLevel)}</h1>
+    </div>
+  );
+}
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join('!');
+}
+
+ReactDOM.render(
+  <HelloReactTypeScript name="Syndesis" enthusiasmLevel={2} />,
+  document.getElementById('syn-root') as HTMLElement
+);
