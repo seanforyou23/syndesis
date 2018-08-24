a64d666f3cc1e44feca389022607bef0ccfcb8b8
Michael Spaxman <seanforyou23@gmail.com>
2018-08-23 20:22:51 -0400

    angular inside of react whaaaaaa

--- app/ui/src/app/app.tsx
+++ app/ui/src/app/app.tsx
@@ -1,6 +1,14 @@
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/observable/from';
 import React from 'react';
 import { Alert } from 'patternfly-react';
 import foo from './test.json';
+import '@atlasmap/atlasmap-data-mapper';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'patternfly-react/dist/css/patternfly-react.css';
 
@@ -9,22 +17,49 @@ export interface Props {
   enthusiasmLevel?: number;
 }
 
-export function App({name, enthusiasmLevel = 1}: Props) {
-  console.log('App.tsx');
-  console.log(foo);
-
-  if (enthusiasmLevel <= 0) {
-    throw new Error('You could be a little more enthusiastic. :D');
+export class App extends React.Component<Props> {
+  public componentDidMount() {
+    import('./ng').then((mod=>{
+      mod.bootstrap();
+    }));
+    // bootstrap();
   }
 
-  return (
-    <div>
-      <h1>{name + getExclamationMarks(enthusiasmLevel)}</h1>
-      <Alert>I am an Alert with type="error"</Alert>
-    </div>
-  );
+  public render () {
+    console.log('App.tsx');
+    console.log(foo);
+
+
+    return (
+      <div>
+        <Alert>I am an Alert with type="error"</Alert>
+        {/* <app-atlasmap-dev-root></app-atlasmap-dev-root> */}
+        {/* <div dangerouslySetInnerHTML={{__html:'<data-mapper></data-mapper>'}} /> */}
+        <div dangerouslySetInnerHTML={{__html:'<test-component></test-component>'}} />
+      </div>
+    );
+  }
 }
 
-function getExclamationMarks(numChars: number) {
-  return Array(numChars + 1).join('!');
-}
+// export function App({name, enthusiasmLevel = 1}: Props) {
+//   console.log('App.tsx');
+//   console.log(foo);
+
+//   if (enthusiasmLevel <= 0) {
+//     throw new Error('You could be a little more enthusiastic. :D');
+//   }
+
+//   return (
+//     <div>
+//       <h1>{name + getExclamationMarks(enthusiasmLevel)}</h1>
+//       <Alert>I am an Alert with type="error"</Alert>
+//       {/* <app-atlasmap-dev-root></app-atlasmap-dev-root> */}
+//       {/* <div dangerouslySetInnerHTML={{__html:'<data-mapper-error></data-mapper-error>'}} /> */}
+//       <div dangerouslySetInnerHTML={{__html:'<test-component></test-component>'}} />
+//     </div>
+//   );
+// }
+
+// function getExclamationMarks(numChars: number) {
+//   return Array(numChars + 1).join('!');
+// }
---
+++ app/ui/src/app/ng.tsx
@@ -0,0 +1,39 @@
+import { NgModule, Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DataMapperModule } from '@atlasmap/atlasmap-data-mapper';
+// import { environment } from 'environments/environment';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+
+@Component({
+  selector: 'test-component',
+  template: `<div>hello world</div>`
+})
+
+export class TestComponent implements OnInit {
+
+  ngOnInit() {
+    console.log('component mounted');
+  }
+
+}
+
+@NgModule({
+  imports: [
+    DataMapperModule,
+    BrowserModule
+  ],
+  bootstrap: [TestComponent],
+  declarations: [TestComponent]
+})
+export class AppModule {}
+
+export function bootstrap() {
+  /* tslint:disable:no-console */
+  platformBrowserDynamic().bootstrapModule(AppModule, {
+    preserveWhitespaces: true
+  })
+  .catch((err: any) => console.log(err));
+
+}
+
