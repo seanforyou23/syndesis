af1c14e905e159c68819ca688e00702152243a3e
Michael Spaxman <seanforyou23@gmail.com>
2018-09-25 22:27:21 -0400

    [issue-3225] add pf-styles
    configure webpack for loading patternfly sass styles, fonts, font icons, and images
    remove dead code
    reuse bits of our old scss scaffolding where it makes sense

--- app/ui/src/app/app.tsx
+++ app/ui/src/app/app.tsx
@@ -1,25 +1,50 @@
 import React from 'react';
 
 import { Alert } from 'patternfly-react';
-
-import 'bootstrap/dist/css/bootstrap.css';
-import 'patternfly-react/dist/css/patternfly-react.css';
+import '../scss/app.scss';
 
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
 import { ApicurioLoader } from './vendor/apicurio/apicurio-loader';
 import { BlockCopyLoader } from './vendor/pfng/block-copy-loader';
+
 export interface Props {
   name: string;
 }
 
+const borderStyles = {
+  border: '1px solid black'
+};
+
 export function App({name}: Props) {
 
   return (
-    <div>
-      <h1>App.tsx Component for: {name}</h1>
-      <Alert>I am an Alert imported from PF-React</Alert>
-      <ApicurioLoader api="fooapi" embedded />
-      <BlockCopyLoader value="copyValue example" />
+    <div className="container">
+      <h1 className="text-center">App.tsx Component for: {name}</h1>
+      <div className="row">
+        <div className="col-xs-6" style={borderStyles}>
+          <h2>Section 1: Import Component from PF-React</h2>
+          <Alert>I am an Alert imported from PF-React</Alert>
+        </div>
+        <div className="col-xs-6" style={borderStyles}>
+          <h2>Section 2: Import Apicurio as WC</h2>
+          <ApicurioLoader api="fooapi" embedded />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-6" style={borderStyles}>
+          <h2>Section 3: Import CopyClipboard from PFNG</h2>
+          <BlockCopyLoader value="copyValue example" />
+        </div>
+        <div className="col-xs-6" style={borderStyles}>
+          <h2>Section 4: Raw PF4 Markup</h2>
+          {/* <div className="pf-c-background-image" /> */}
+          {/* https://pf-next.com/components/Button/examples/ */}
+          <button className="pf-c-button pf-m-primary">
+            Primary
+          </button>
+        </div>
+      </div>
+
     </div>
   );
 }
