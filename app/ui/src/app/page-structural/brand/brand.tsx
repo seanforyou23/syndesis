07942703e38c20c9937c048159c14c9e88bd167a
Michael Spaxman <seanforyou23@gmail.com>
2018-10-02 19:55:40 -0400

    [issue-3225] add layout
    turn off pf3 and web component stuff
    get a general page layout going
    better sourcemaps
    build some simple components for header, sidebar, and main

---
+++ app/ui/src/app/page-structural/brand/brand.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import logoSymbol from '../../../assets/images/glasses_logo.svg';
+import logoText from '../../../assets/images/syndesis-logo-svg-white.svg';
+
+export class Brand extends React.Component {
+
+  toggleNav(): void {
+    (function() {
+      const mq = window.matchMedia('(min-width: 768px)');
+      let page = document.getElementById('primary-nav');
+      page = page.parentElement;
+      if (mq.matches) {
+          page.classList.remove('pf-m-expanded');
+          page.classList.toggle('pf-m-collapsed');
+      } else {
+          page.classList.remove('pf-m-collapsed');
+          page.classList.toggle('pf-m-expanded');
+      }
+      return false;
+  })();
+  // return false;
+  }
+
+  render() {
+    return (
+      <div className="pf-l-page__header-brand">
+        <div className="pf-l-page__header-brand-toggle">
+          <button
+            className="pf-c-button pf-m-plain"
+            id="nav-toggle"
+            aria-label="Toggle primary navigation"
+            onClick={() => {this.toggleNav()}}>
+            <i className="fas fa-bars" aria-hidden="true"></i>
+          </button>
+        </div>
+        <a className="pf-l-page__header-brand-link">
+          <img className="pf-c-brand pf-m-sm" src={logoSymbol} alt="Syndesis Logo Symbol" />
+          <img className="pf-c-brand pf-m-sm" src={logoText} alt="Syndesis Logo Text" />
+        </a>
+      </div>
+    );
+  }
+}
+
+
+// export function Brand() {
+//   return (
+//     <div className="pf-l-page__header-brand">
+//       <div className="pf-l-page__header-brand-toggle">
+
+//         <button className="pf-c-button pf-m-plain" id="nav-toggle" aria-label="Toggle primary navigation">
+//           <i className="fas fa-bars" aria-hidden="true"></i>
+//         </button>
+
+
+//       </div>
+//     </div>
+//   );
+// }
