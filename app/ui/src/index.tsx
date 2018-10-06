b2c69a1bbf2ec2bc1bc758800b7845eea6da3c06
Michael Spaxman <seanforyou23@gmail.com>
2018-10-06 10:58:10 -0400

    [issue-3225] move axe around, still having issues

--- app/ui/src/index.tsx
+++ app/ui/src/index.tsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './app/app';
 
-if (process.env.NODE_ENV !== 'production') {
-  var axe = require('react-axe');
-  axe(React, ReactDOM, 1000);
-}
-
 ReactDOM.render(
   <BrowserRouter>
     <App appName="Syndesis" />
