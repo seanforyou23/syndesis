2b6f3a26a1fc8e5310ff85db2542635ff21a7313
Michael Spaxman <seanforyou23@gmail.com>
2018-10-06 10:58:10 -0400

    [issue-3225] move axe around, still having issues

--- app/ui/src/app/app.tsx
+++ app/ui/src/app/app.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import '../scss/app.scss';
 import { MetadataState } from './domains/platform/types/platform.models';
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
@@ -6,7 +7,10 @@ import { Chrome } from './components/chrome/chrome';
 import { Header } from './components/chrome/header/header';
 import { Sidebar } from './components/chrome/sidebar/sidebar';
 import { Routes } from './routes';
-
+var axe = require('react-axe');
+if (process.env.NODE_ENV !== 'production') {
+  axe(React, ReactDOM, 1000);
+}
 export class App extends React.Component<MetadataState> {
 
   constructor(props: MetadataState) {
