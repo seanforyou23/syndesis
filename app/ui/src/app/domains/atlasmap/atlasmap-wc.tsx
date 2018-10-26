4b7dd18d81a4270e7865e8f3f073bdf022b0ff4f
Riccardo Forina <rforina@redhat.com>
2018-10-26 14:47:25 +0200

    Load atlasmap as a wc, and add some useful info to the loading component in case of errors

--- app/ui/src/app/domains/atlasmap/atlasmap-wc.tsx
+++ app/ui/src/app/domains/atlasmap/atlasmap-wc.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Main } from '../../components/main/main';
-// import '@atlasmap/atlasmap-data-mapper/atlasmap-wc';
+import '@atlasmap/atlasmap-wc';
 
 export default class AtlasmapComponent extends React.Component {
   render() {
-    // return <atlasmap-wc></atlasmap-wc>
     return (
       <Main>
         <h1>lazy loaded atlasmap</h1>
+        <atlasmap-wc></atlasmap-wc>
       </Main>
     );
   }
